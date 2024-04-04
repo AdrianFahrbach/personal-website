d4b79f1bd5fa29153ecf3c9a82ed5d0bc2f54e30
Adrian Fahrbach <adrian@hatchery.io>
2024-04-05 00:39:17 +0200

    ♻️ refactor: move headers to next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
-const path = require('path')
- 
+const path = require('path');
+
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-}
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Link',
+            value: '<https://prod.spline.design>; rel=preconnect;',
+          },
+        ],
+      },
+    ];
+  },
+};
