eea3334ebfe71084acac4a74c578178933f76152
Adrian Fahrbach <adrianfahrbach@me.com>
2025-11-16 19:36:52 +0100

    feat(AnimatedNumber): add wrapping of numbers

--- src/components/v2/AnimatedNumber/AnimatedNumber.tsx
+++ src/components/v2/AnimatedNumber/AnimatedNumber.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import NumberFlow, { continuous } from '@number-flow/react';
 import styles from './AnimatedNumber.module.scss';
 import classNames from 'classnames';
 
@@ -8,58 +9,31 @@ interface AnimatedNumberProps {
   className?: string;
 }
 
-const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
 export const AnimatedNumber: React.FC<AnimatedNumberProps> = ({
   value,
   decimals = 2,
   className,
 }) => {
-  const formattedValue = value.toFixed(decimals);
-  const characters = formattedValue.split('');
-
   return (
-    <div className={classNames(styles.animatedNumber, className)}>
-      {characters.map((char, index) => {
-        const isDigit = !isNaN(parseInt(char));
-        const isDecimalPoint = char === '.';
-        const isMinus = char === '-';
-
-        if (isDecimalPoint || isMinus) {
-          return (
-            <div
-              key={index}
-              className={classNames(styles.digitContainer, {
-                [styles.isDecimalPoint]: isDecimalPoint,
-                [styles.isMinus]: isMinus,
-              })}
-            >
-              <div className={styles.staticChar}>{char}</div>
-            </div>
-          );
-        }
-
-        // For digits, create a column of all numbers 0-9
-        const digitValue = parseInt(char);
-        const offset = -digitValue * 1.2; // Each digit is 1.2em height
-
-        return (
-          <div key={index} className={styles.digitContainer}>
-            <div
-              className={styles.digitColumn}
-              style={{
-                transform: `translateY(${offset}em)`,
-              }}
-            >
-              {DIGITS.map((digit) => (
-                <div key={digit} className={styles.digit}>
-                  {digit}
-                </div>
-              ))}
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <NumberFlow
+      value={value}
+      format={{
+        minimumFractionDigits: decimals,
+        maximumFractionDigits: decimals,
+      }}
+      locales="en-US"
+      willChange
+      isolate
+      plugins={[continuous]}
+      opacityTiming={{
+        duration: 250,
+        easing: 'ease-out',
+      }}
+      transformTiming={{
+        easing: 'cubic-bezier(0.4, 0.0, 0.2, 1)',
+        duration: 300,
+      }}
+      className={classNames(styles.animatedNumber, className)}
+    />
   );
 };
